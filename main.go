7cf141cb5fb5e14e49ff3f32637428d293f8dea6
Andrew Stuart <andrew.stuart2@gmail.com>
2016-12-07 11:22:52 -0700

    Update with license, add glide, add print-only

--- main.go
+++ main.go
@@ -1,3 +1,17 @@
+// Copyright 2016 Andrew Stuart
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -8,6 +22,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/pkg/api/v1"
@@ -15,16 +30,17 @@ import (
 )
 
 const (
-	LabelSAMLMetadataEndpoint = "k8s.unicon.net/saml-metadata-endpoint"
-	LabelSAMLMetadataPort     = "k8s.unicon.net/saml-metadata-port"
-	LabelSAMLMetadataProtocol = "k8s.unicon.net/saml-metadata-protocol"
+	annotSAMLMetadataEndpoint      = "k8s.unicon.net/saml-metadata-endpoint"
+	annotationSAMLMetadataPort     = "k8s.unicon.net/saml-metadata-port"
+	annotationSAMLMetadataProtocol = "k8s.unicon.net/saml-metadata-protocol"
 
-	DefaultPort     = "8080"
-	DefaultProtocol = "http"
+	defaultPort  = "8080"
+	defaultProto = "http"
 )
 
 var (
-	host = flag.String("host", "", "Set a custom kubernetes host. If unset, defaults to in-cluster config")
+	host      = flag.String("host", "", "Set a custom kubernetes host. If unset, defaults to in-cluster config")
+	printOnly = flag.Bool("print-only", false, fmt.Sprintf("Set %s to only print out pod URLS that would have been collected."))
 )
 
 func init() {
@@ -52,36 +68,49 @@ func main() {
 		log.Fatal(err)
 	}
 
-	pods, err := cli.Pods("").List(v1.ListOptions{
-		FieldSelector: "metadata.annotations." + LabelSAMLMetadataEndpoint,
-	})
+	for {
+		pods, err := cli.Pods("").List(v1.ListOptions{})
 
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for i := range pods.Items {
-		res, err := http.Get(getURL(&pods.Items[i]))
 		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
+			log.Fatal(err)
 		}
-		io.Copy(os.Stdout, res.Body)
-		res.Body.Close()
+
+		for i := range pods.Items {
+			if pods.Items[i].Annotations[annotSAMLMetadataEndpoint] == "" {
+				continue
+			}
+
+			url := getURL(&pods.Items[i])
+
+			fmt.Fprintln(os.Stderr, "Retrieving URL", url)
+			if *printOnly {
+				continue
+			}
+
+			res, err := http.Get(url)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+			}
+			io.Copy(os.Stdout, res.Body)
+			res.Body.Close()
+		}
+
+		time.Sleep(10 * time.Second)
 	}
 }
 
 func getURL(p *v1.Pod) string {
-	port := p.Labels[LabelSAMLMetadataPort]
+	port := p.Annotations[annotationSAMLMetadataPort]
 	if port == "" {
-		port = DefaultPort
+		port = defaultPort
 	}
 
-	proto := p.Labels[LabelSAMLMetadataProtocol]
+	proto := p.Annotations[annotationSAMLMetadataProtocol]
 	if proto == "" {
-		proto = DefaultProtocol
+		proto = defaultProto
 	}
 
-	endpoint := strings.TrimLeft(p.Labels[LabelSAMLMetadataEndpoint], "/")
+	endpoint := strings.TrimLeft(p.Annotations[annotSAMLMetadataEndpoint], "/")
 
 	return fmt.Sprintf("%s://%s:%s/%s", proto, p.Status.PodIP, port, endpoint)
 }
